fcba7783eaac3dd28df6736114d5f0ebb32c4810
mullunani2-ai <mullunani2@gmail.com>
2026-07-22 02:13:53 -0700

    Add files via upload

---
+++ large.py
@@ -0,0 +1,8 @@
+n=int(input("enter the gender"))
+if avg>=60:
+    if gender is male:
+        print("eligible")
+    else gender is female:
+        print("not eligible")
+else:
+    print("not eligible")
